0982341f88dd882059a13c4c2d680d7280600ab2
BadJoKing <badatjoking@gmail.com>
2025-07-01 21:02:15 +0200

    remove exceptions

--- InputListener.java
+++ InputListener.java
@@ -9,22 +9,19 @@ public class InputListener implements KeyListener{
             case KeyEvent.VK_UP:
                 
                 break;
-        
+            
             default:
                 break;
         }
+        
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
     
 }
