3293ac1e62c218f407ef4de7401f10cc1c0e0bff
BadJoKing <badatjoking@gmail.com>
2025-07-01 21:01:07 +0200

    Make Entity do entity things

--- Entity.java
+++ Entity.java
@@ -1,3 +1,19 @@
-public abstract class Entity{
-    public abstract void move();
+import javax.swing.ImageIcon;
+
+public class Entity extends ImageIcon{
+    private int x,y;
+
+
+    public Entity(String path){
+        super(path);
+    }
+
+    public void move(int dx,int dy){
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public int[] getPos(){
+        return new int[] {this.x, this.y};
+    }
 }
